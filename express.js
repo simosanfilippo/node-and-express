66cf26a0211c73ed9ef3793339abe5b1beba8f63
simosanfilippo <simone.sanfilippo@diemmea.com>
2022-05-11 18:36:29 +0200

    Filter by Id and node_modules ignored

--- express.js
+++ express.js
@@ -11,5 +11,15 @@ app.get('/api/products', async (req, res) => {
 // Creare un nuovo endpoint che, dato un url tipo: /api/v1/products/123 restituisce solo il prodotto specifico con id "123"
 // gitignore node_modules
 
+app.get('/api/products/:productId', async (req, res) => {
+    const products = await fsController.readProducts()
+    for (singleproduct of products){  
+        if (singleproduct.id == req.params.productId){
+            res.status(200).json(singleproduct)
+        }  
+    }
+    res.status(204).end()
+})
+
 const port = 8080
 app.listen(port, () => console.log(`listening on ${port}`))
