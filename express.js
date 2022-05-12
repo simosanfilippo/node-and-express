6da3f9874c4215eaa2bb8c19ecb3760fb22ca541
simosanfilippo <simone.sanfilippo@diemmea.com>
2022-05-12 18:19:48 +0200

    Aggiunta Router per Prodotti

--- express.js
+++ express.js
@@ -1,28 +1,28 @@
 const express = require('express')
 const app = express();
 
-const fsController = require ('./server.js')
-
-app.get('/api/products', async (req, res) => {
-    const products = await fsController.readProducts()
-    res.status(200).json(products)
-})
-
 // Creare un nuovo endpoint che, dato un url tipo: /api/v1/products/123 restituisce solo il prodotto specifico con id "123"
 // gitignore node_modules
-
-app.get('/api/products/:productId', async (req, res) => {
-    const products = await fsController.readProducts()
-    for (singleproduct of products){  
-        if (singleproduct.id == req.params.productId){
-            res.status(200).json(singleproduct)
-        }  
-    }
-    res.status(204).end()
-})
 
 const port = 8080
 app.listen(port, () => console.log(`listening on ${port}`))
 
-const wiki = require('./wiki.js');
-app.use('/wiki', wiki);
+const productsRoute = require('./products.js');
+app.use('/api/products/', productsRoute);
+
+var db = require("./database.js")
+
+app.get("/api/prodottiDB", (req, res, next) => {
+    var sql = "select * from user"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+      });
+});
